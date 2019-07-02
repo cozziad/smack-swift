adbc5bd9116c8ac3aa5edd1d62c10d0efb1c03a5
Anthony <cozziad@gmail.com>
2019-07-02 18:51:12 -0400

    Lecture 065 Done

--- smack/Controller/ChatViewController.swift
+++ smack/Controller/ChatViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
