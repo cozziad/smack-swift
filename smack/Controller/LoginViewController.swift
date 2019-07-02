2944eb0545e27b0405041106537235e541266d67
Anthony <cozziad@gmail.com>
2019-07-02 19:57:57 -0400

    Lecture 066 Done

---
+++ smack/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  smack
+//
+//  Created by Anthony Cozzi on 7/2/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Anthony Cozzi on 7/2/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
+
