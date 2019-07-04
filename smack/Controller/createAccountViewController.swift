a0cc4419682dc2e59c3b6be6cee53166cca64493
Anthony <cozziad@gmail.com>
2019-07-03 21:37:41 -0400

    Lecture 72 Done

--- smack/Controller/createAccountViewController.swift
+++ smack/Controller/createAccountViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class createAccountViewController: UIViewController {
 
+    //Outlets
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,5 +28,18 @@ class createAccountViewController: UIViewController {
         performSegue(withIdentifier: FROM_CREATE_ACCOUNT_UNWIND, sender: nil)
     }
     
-
+    @IBAction func createAccountPressed(_ sender: Any) {
+        print("press detected")
+        guard let email = emailTxt.text, emailTxt.text != "" else {return}
+        guard let pass = passwordTxt.text, passwordTxt.text != "" else {return}
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success{ print("registered user")}
+            else {print("failed register")}
+        }
+    }
+    @IBAction func chooseAvitarPressed(_ sender: Any) {
+    }
+    @IBAction func generateBackgroundPressed(_ sender: Any) {
+    }
+    
 }
--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -11,7 +11,8 @@ import Foundation
 typealias CompletionHandler = (_ Success:Bool) -> ()
 
 //URL Constants
-let BASE_URL = "http://10.0.2.2:3005/v1/"
+//let BASE_URL = "http://10.0.2.2:3005/v1/"
+let BASE_URL = "http://localhost:3005/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 
 //Segues
