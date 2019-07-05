69ba279474da0aa21945849c927132f3880ed8bb
Anthony <cozziad@gmail.com>
2019-07-05 09:56:59 -0400

    Lecture 082 Done

--- smack/Controller/ChannelViewController.swift
+++ smack/Controller/ChannelViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class ChannelViewController: UIViewController {
+class ChannelViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     //Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
     @IBAction func prepareForUnwind (segue:UIStoryboardSegue){}
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       tableView.delegate = self
+        tableView.dataSource = self
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.width-60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
@@ -54,5 +58,25 @@ class ChannelViewController: UIViewController {
             userImg.backgroundColor = UIColor.clear
         }
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? TableViewCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            return cell
+        }
+        else{
+            return UITableViewCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    
 
 }
---
+++ smack/View/TableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewCell.swift
+//  smack
+//
+//  Created by Anthony Cozzi on 7/5/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected{
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        }
+        else{
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell (channel: Channel)
+    {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+
+}
